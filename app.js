714b5afa2c6696b9748453805432de3f770b8dd0
Ultrax92 <kiwiihs@gmail.com>
2025-06-17 11:04:08 +0200

    Creation of the Node.js api

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Doe" },
-];
+// TP 1
 
-app.get("/users", (req, res) => {
-  res.json(users);
+// const users = [
+//   { id: 1, name: "John Doe" },
+//   { id: 2, name: "Jane Doe" },
+// ];
+
+// app.get("/users", (req, res) => {
+//   res.json(users);
+// });
+
+// app.delete("/user/:id", (req, res) => {
+//   const userId = parseInt(req.params.id);
+//   const index = users.findIndex((user) => user.id === userId);
+//   if (index !== -1) {
+//     users.splice(index, 1);
+//     res.status(200).send(`User with ID ${userId} deleted`);
+//   } else {
+//     res.status(404).send(`User not found`);
+//   }
+// });
+
+
+// TP 2 
+
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.delete("/user/:id", (req, res) => {
-  const userId = parseInt(req.params.id);
-  const index = users.findIndex((user) => user.id === userId);
-  if (index !== -1) {
-    users.splice(index, 1);
-    res.status(200).send(`User with ID ${userId} deleted`);
-  } else {
-    res.status(404).send(`User not found`);
-  }
+app.get('status', (req, res) => {
+  res.json({ status: 'API is running' });
 });
 
+module.exports = app;
